828c0f1710ed0a4e349c4941b1446053c55985b2
Prathap0642h <saiprathap0406@gmail.com>
2026-04-06 15:40:39 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,18 @@
-<?php session_start(); if(!isset($_SESSION['user'])) header("Location: login.php"); ?>
-<!DOCTYPE html><html><head><link rel="stylesheet" href="style.css"></head><body>
-<div class="card" style="text-align:center;">
-    <h1>Welcome, <?php echo $_SESSION['user']; ?></h1>
-    <p>Please select an action below to proceed.</p>
-    <a href="quiz.php" class="btn">Start New Quiz</a><br><br>
-    <a href="logout.php" style="color:#94a3b8">Logout</a>
-</div></body></html>
+<?php
+session_start();
+// If already logged in, show the dashboard
+if(isset($_SESSION['user'])) {
+    ?>
+    <!DOCTYPE html><html><head><link rel="stylesheet" href="style.css"></head><body>
+    <div class="card" style="text-align:center;">
+        <h1>Welcome, <?php echo $_SESSION['user']; ?></h1>
+        <p>Please select an action below to proceed.</p>
+        <a href="quiz.php" class="btn">Start New Quiz</a><br><br>
+        <a href="logout.php" style="color:#94a3b8">Logout</a>
+    </div></body></html>
+    <?php
+} else {
+    // If NOT logged in, immediately show the login page
+    include 'login.php';
+}
+?>
